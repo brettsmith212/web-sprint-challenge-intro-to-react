6544be0565602c1bbf4522b62b1ee865b1a8510b
Brett Smith <brettsmith212@gmail.com>
2021-12-10 08:29:33 -0800

    characters on page

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,17 +2,24 @@
 import React from "react";
 import styled from "styled-components";
 
+const CharacterWrapper = styled.div``;
+
 function Character(props) {
   const { data } = props;
   console.log(data);
-  data.map((person) => {
-    return person.name;
+
+  const characterArr = data.map((person) => {
+    return (
+      <CharacterWrapper>
+        <p>{person.name}</p>
+        <p>{person.height}</p>
+        <p>{person.mass}</p>
+        <p>{person.hair_color}</p>
+      </CharacterWrapper>
+    );
   });
-  return (
-    <div>
-      <h2>data</h2>
-    </div>
-  );
+
+  return <div>{characterArr}</div>;
 }
 
 export default Character;
