a9a70be26edb8ffb505273f760ab4b7515bb59bb
Brett Smith <brettsmith212@gmail.com>
2021-12-10 08:21:43 -0800

    axios fetching data correctly

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,8 +1,18 @@
 // Write your Character component here
 import React from "react";
+import styled from "styled-components";
 
-function Character() {
-  return <div></div>;
+function Character(props) {
+  const { data } = props;
+  console.log(data);
+  data.map((person) => {
+    return person.name;
+  });
+  return (
+    <div>
+      <h2>data</h2>
+    </div>
+  );
 }
 
 export default Character;
